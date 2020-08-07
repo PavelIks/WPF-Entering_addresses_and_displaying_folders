c79e6b493d2b14a9cef57223d89e17322c32f682
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-07 22:51:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace AddressFolders
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*var path = @"D:\Programs";*/
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var path = @"" + TextBox1.Text + "";
+                string[] normies = new string[Directory.GetDirectories(path).Length];
+                for (int i = 0; i < normies.Length; i++)
+                {
+                    normies[i] = Directory.GetDirectories(path)[i]/*.Split('\\')[0]*/;
+                }
+                ComboBox1.ItemsSource = normies;
+
+                Label1.Visibility = Visibility;
+                Label1.Content = "Путь указан!";
+            }
+            catch
+            {
+                Label1.Visibility = Visibility;
+                Label1.Content = "Неверный путь!";
+            }
+        }
+    }
+}
